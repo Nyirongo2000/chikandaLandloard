9d2540794f35a0d0b29527e772944846f4771832
oliver nyirongo <bed-com-21-19@unima.ac.mw>
2023-07-26 16:08:58 -0700

    remaining with icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,28 @@
 import React from "react";
 import { useState } from "react";
 import "../App.css";
-
+// import ReorderIcon from "@material-ui/icons/Reorder";
+// import SearchIcon from "@material-ui/icons/Search";
 function Navbar() {
 
-    // const[showLinks, setShowLinks]=useState[false];
+  const [showLinks, setShowLinks] = useState(false);
 
   return (
     <div className="Navbar">
       <div className="leftSide">
-        <div className="links">
+        {/* if showlinks =true then set id to hidden else empty id */}
+        <div className="links" id={showLinks ? "hidden" : ""}>
           <a href="/home">Home</a>
           <a href="/book">Book</a>
           <a href="/about">about us</a>
           <a href="/contact">Contact Us</a>
         </div>
-        <button>open</button>
+        {/* sets to the oposite of initial on click */}
+        <button onClick={()=> setShowLinks(!showLinks)}>open</button>
       </div>
       <div className="rightSide">
         <input type="text" placeholder="search..."/>
-        <button>Search</button>
+        <button>SearchIcon</button>
       </div>
     </div>
   );
