3b1798c41418e4edeff4c71661ef3290d179fb8b
oliver nyirongo <bed-com-21-19@unima.ac.mw>
2023-07-27 17:29:26 -0700

    liking

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
-
+import Layout from './pages/Layout';
+import Hostels from './pages/Hostels';
+import AboutUs from './pages/AboutUs';
+import Footer from './components/Footer';
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-    </div>
+    <BrowserRouter> 
+    <Navbar/>
+        <Routes>
+          <Route path="/" element={<Layout/>}> </Route>
+          <Route path="about" element={<AboutUs />} />
+          <Route path="hostels" element={<Hostels/>}/>
+          {/* <Route path="blog" element={<Blog/>}/> */}
+          {/* <Route path="magazineupload" element={<MagazineUpload/>}/> */}
+        </Routes>
+    <Footer/>
+    </BrowserRouter>
   );
 }
 
