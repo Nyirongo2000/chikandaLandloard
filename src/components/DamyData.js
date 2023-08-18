0a0749194d1ad6e8b158b65217c7a40d68681df3
oliver nyirongo <bed-com-21-19@unima.ac.mw>
2023-08-17 20:13:40 -0700

    popup to view more details done

--- src/components/DamyData.js
+++ src/components/DamyData.js
@@ -2,7 +2,9 @@ import React from 'react'
 import './DammyData.css'
 import Hostel from "../resources/hostel.jpg";
 import PopUpDetails from './PopUpDetails';
+import { useState } from 'react';
 const DamyData = () => {
+  const [buttonPopup,setButtonPopup]= useState(false);
   return (
     <div>
       <div id="makeFull">
@@ -18,26 +20,82 @@ const DamyData = () => {
               <p>minutes:5</p>
             </div>
             <button>info </button>
-            <button>more info</button>
-            <PopUpDetails trigger={true}>
+            <button
+              className="close-modal"
+              onClick={() => setButtonPopup(true)}
+            >
+              more info
+            </button>
+            <PopUpDetails trigger={buttonPopup} setTrigger={setButtonPopup}>
               <h2>my popup</h2>
+              <div>
+                <div className="leftDetails">
+                  <p> image slider</p>
+                  <img src={Hostel} alt="Developer1" />
+                </div>
+                <div className="rightDetails"></div>
+              </div>
             </PopUpDetails>
-          </div>
-        </div>
-        <div class="gallery">
-          <a target="_blank" href="../data/dumy.jpg">
-            <img src={Hostel} alt="Developer1" />
-          </a>
-          <div class="desc  dark:text-gray-400">
-            bsc computer science, profficient in FullStack Development
           </div>
         </div>
         <div class="gallery">
           <a target="_blank" href="img_mountains.jpg">
             <img src={Hostel} alt="Developer1" />
           </a>
-          <div class="desc  dark:text-gray-400">
-            bsc computer science, profficient in FullStack Development
+          <div className="text-red-400">
+            <div>
+              <p>Gender:</p>
+              <p>double rooms:5</p>
+              <p>minutes:5</p>
+              <p>minutes:5</p>
+            </div>
+            <button>info </button>
+            <button
+              className="close-modal"
+              onClick={() => setButtonPopup(true)}
+            >
+              more info
+            </button>
+            <PopUpDetails trigger={buttonPopup} setTrigger={setButtonPopup}>
+              <h2>my popup</h2>
+              <div>
+                <div className="leftDetails">
+                  <p> image slider</p>
+                  <img src={Hostel} alt="Developer1" />
+                </div>
+                <div className="rightDetails"></div>
+              </div>
+            </PopUpDetails>
+          </div>
+        </div>
+        <div class="gallery">
+          <a target="_blank" href="img_mountains.jpg">
+            <img src={Hostel} alt="Developer1" />
+          </a>
+          <div className="text-red-400">
+            <div>
+              <p>Gender:</p>
+              <p>double rooms:5</p>
+              <p>minutes:5</p>
+              <p>minutes:5</p>
+            </div>
+            <button>info </button>
+            <button
+              className="close-modal"
+              onClick={() => setButtonPopup(true)}
+            >
+              more info
+            </button>
+            <PopUpDetails trigger={buttonPopup} setTrigger={setButtonPopup}>
+              <h2>my popup</h2>
+              <div>
+                <div className="leftDetails">
+                  <p> image slider</p>
+                  <img src={Hostel} alt="Developer1" />
+                </div>
+                <div className="rightDetails"></div>
+              </div>
+            </PopUpDetails>
           </div>
         </div>
       </div>
--- src/components/PopUpDetails.js
+++ src/components/PopUpDetails.js
@@ -4,7 +4,7 @@ function PopUpDetails(props) {
   return (props.trigger) ? (
     <div className="popup">
       <div className="popup-inner">
-        <button className="close-btn">close</button>
+        <button className="close-btn" onClick={() => props.setTrigger(false)}>close</button>
         {props.children}
       </div>
     </div>
